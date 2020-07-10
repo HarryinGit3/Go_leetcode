b239e045432586eb76d867af6cb1a11db6b890eb
harryin <harryin123@qq.com>
2020-07-10 22:04:11 +0800

    leetcode309解法

--- 202007/leetcode309.go
+++ 202007/leetcode309.go
@@ -2,4 +2,51 @@
 * @Author: YinHao
 * @Date: 2020/7/10 18:48
  */
-package _02007
+
+/*
+
+给定一个整数数组，其中第 i 个元素代表了第 i 天的股票价格 。​
+
+设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+
+你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+示例:
+
+输入: [1,2,3,0,2]
+输出: 3
+解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+
+
+*/
+package main
+
+import "fmt"
+
+func maxProfit(prices []int) int {
+	if len(prices) == 0 {
+		return 0
+	}
+	n := len(prices)
+	f := make([][3]int, n)
+	f[0][0] = -prices[0]
+	for i := 1; i < n; i++ {
+		f[i][0] = max(f[i-1][0], f[i-1][2]-prices[i])
+		f[i][1] = f[i-1][0] + prices[i]
+		f[i][2] = max(f[i-1][1], f[i-1][2])
+	}
+	return max(f[n-1][1], f[n-1][2])
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	prices := []int{1, 2, 3, 0, 2}
+	fmt.Println(maxProfit(prices))
+
+}
